71e7f67b55f58032c2e0304d527364b2af222c67
Felix <felixfong03@live.com>
2021-05-16 15:47:06 -0400

    yeet

--- src/model/JourneyToWork.java
+++ src/model/JourneyToWork.java
@@ -14,66 +14,34 @@ public class JourneyToWork extends MyDataset {
      */
     
     @Override
-    public void setDataset (ArrayList<ArrayList<String>> dataset) {
-    
-        final int CITY_INDEX = 3;
-        final String TOT_ = "TOT_";
-    
-        // Index all of the cities
-        for (int i = 1; i<dataset.size(); ++i) {
-            
-            String city = dataset.get(i).get(CITY_INDEX);
-            if (!getDataset().containsKey(city)) {
-                getDataset().put(city, new HashMap<>(dataset.get(CITY_INDEX).size()));
-            }
-            // Increment the number of times the city was entered
-            getCityCount().put(city, getCityCount().getOrDefault(city, 0)+1);
-            
-        }
+    public void indexDataset (ArrayList<ArrayList<String>> dataset, HashSet<String> cities) {
         
         // Get and organize the columns
         ArrayList<String> categoryRow = dataset.get(0);
-        ArrayList<Integer> groups = new ArrayList<>();
         String group = null;
-        
+    
+        // Index all of groups and categories
         for (int i = CITY_INDEX+1; i<categoryRow.size(); ++i) {
-            
+        
             String category = categoryRow.get(i);
-            
-            // If there is TOT_, then create a new group
+        
+            // If there is TOT_ then create a new group
             if (category.contains(TOT_)) {
                 group = category.replace(TOT_, "");
-                for (Map.Entry<String, HashMap<String, TreeMap<String, ArrayList<Double>>>> city: getDataset().entrySet()) {
-                    city.getValue().put(group, new TreeMap<>());
-                }
-                groups.add(i);
-            } else {
-                for (Map.Entry<String, HashMap<String, TreeMap<String, ArrayList<Double>>>> city: getDataset().entrySet()) {
-                    city.getValue().get(group).put(category, new ArrayList<>());
-                }
+                getDataset().put(group, new TreeMap<>());
+                continue;
+                
+            } else if (category.contains(SHAPE__) && !group.equals("Shape")) {
+                group = "Shape";
+                getDataset().put(group, new TreeMap<>());
             }
-            
-        }
-    
-        // put the data into dataset
-        for (int row = 1; row<dataset.size(); ++row) {
-            for (int col = CITY_INDEX+1; col<dataset.get(row).size(); ++col) {
-
-                if (dataset.get(0).get(col).contains(TOT_)) continue;
-                // Find the group the current category is in with binary search
-                try {
-                    getDataset().get(dataset.get(row).get(CITY_INDEX))
-                            .get(dataset.get(0).get(groups.get(binarySearch(groups, col))).replace(TOT_, ""))
-                            .get(dataset.get(0).get(col))
-                            .add(Double.parseDouble(dataset.get(row).get(col)));
-                } catch (NullPointerException e) {
-                    System.err.println("not good");
-                }
-
+        
+            getDataset().get(group).put(category, new HashMap<>());
+            for (String city: cities) {
+                getDataset().get(group).get(category).put(city, 0.0);
             }
+        
         }
-    
-        System.out.println("hi");
     
     }
     
--- src/model/ProfileOfHousing.java
+++ src/model/ProfileOfHousing.java
@@ -1,35 +1,56 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class ProfileOfHousing extends MyDataset {
     
-//    private int ObjectID;
-//    private int DAUID;
-//    private int CSDUID;
-//    private String CsdName;
-//    private int TOT_PVT_HH_TENURE;
-//    private int owner;
-//    private int renter;
-//    private int bandHousing;
-//    private int TOT_OCC_PVT_DWELL_CONDO;
-//    private int condo;
-//    private int notCondo;
-//    private int TOT_OCC_PVT_DWELL_NO_BED;
-//    private int NO_BED;
-//    private int ONE_BED;
-    
+    /*
+        OWN_MED_MONTHLY_SHELTER_COST
+        OWN_AVG_MONTHLY_SHELTER_COST
+        MED_DWELLING_VALUE
+        AVG_DWELLING_VALUE
+        MED_MONTHLY_SHELTER_RENT
+        AVG_MONTHLY_SHELTER_RENT
+     */
+    public static final String MED_ = "MED_";
     
     public ProfileOfHousing () {
         super();
+        getGroupIndicators().add(MED_);
     }
     
     @Override
-    public void setDataset (ArrayList<ArrayList<String>> dataset) {
-    
-    
-    
+    public void indexDataset (ArrayList<ArrayList<String>> dataset, HashSet<String> cities) {
+        
+        // Get and organize the columns
+        ArrayList<String> categoryRow = dataset.get(0);
+        String group = null;
+        
+        // Index all of groups and categories
+        for (int i = CITY_INDEX+1; i<categoryRow.size(); ++i) {
+            
+            String category = categoryRow.get(i);
+            
+            // If there is TOT_ then create a new group and skip
+            if (category.contains(TOT_)) {
+                
+                group = category.replace(TOT_, "");
+                getDataset().put(group, new TreeMap<>());
+                continue;
+                
+            } else if (category.contains(SHAPE__) && !group.equals("Shape") || category.contains(MED_)) {
+                group = category.contains(SHAPE__)
+                        ? "Shape":category.replace(MED_, "");
+                getDataset().put(group, new TreeMap<>());
+            }
+            
+            getDataset().get(group).put(category, new HashMap<>());
+            for (String city : cities) {
+                getDataset().get(group).get(category).put(city, 0.0);
+            }
+            
+        }
+        
     }
-
+    
 }
