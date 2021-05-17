78f6f9af3e0766e635afc52335babb2b4a02b57d
brandon-vo <76707560+brandon-vo@users.noreply.github.com>
2021-05-17 11:33:57 -0400

    Brandon

--- src/view/DensityMapGUI.java
+++ src/view/DensityMapGUI.java
@@ -14,14 +14,14 @@ public class DensityMapGUI extends Tool {
             "Household Renters"};
     private JComboBox dataList = new JComboBox<>(dataOptions);
     private JButton submitButton = new JButton("SUBMIT");
-    private JLabel aurora = new JLabel(new ImageIcon("img/aurora.png"));
-    private JLabel eastGwillimbury = new JLabel(new ImageIcon("img/eastGwillimbury.png"));
-    private JLabel georgina = new JLabel(new ImageIcon("img/georgina.png"));
-    private JLabel king = new JLabel(new ImageIcon("img/king.png"));
-    private JLabel markham = new JLabel(new ImageIcon("img/markham.png"));
-    private JLabel newmarket = new JLabel(new ImageIcon("img/newmarket.png"));
-    private JLabel richmondhill = new JLabel(new ImageIcon("img/richmondhill.png"));
-    private JLabel stouffville = new JLabel(new ImageIcon("img/stouffville.png"));
+    private JLabel aurora = new JLabel(new ImageIcon("img/map/aurora.png"));
+    private JLabel eastGwillimbury = new JLabel(new ImageIcon("img/map/eastGwillimbury.png"));
+    private JLabel georgina = new JLabel(new ImageIcon("img/map/georgina.png"));
+    private JLabel king = new JLabel(new ImageIcon("img/map/king.png"));
+    private JLabel markham = new JLabel(new ImageIcon("img/map/markham.png"));
+    private JLabel newmarket = new JLabel(new ImageIcon("img/map/newmarket.png"));
+    private JLabel richmondhill = new JLabel(new ImageIcon("img/map/richmondHill.png"));
+    private JLabel stouffville = new JLabel(new ImageIcon("img/map/stouffville.png"));
 
     public DensityMapGUI() {
 
