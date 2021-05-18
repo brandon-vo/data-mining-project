6d244d989b241c1bfe116e89268ced2c15a8e135
brandon-vo <76707560+brandon-vo@users.noreply.github.com>
2021-05-18 12:52:45 -0400

    Brandon map

--- src/view/DensityMapGUI.java
+++ src/view/DensityMapGUI.java
@@ -6,9 +6,10 @@ import org.w3c.dom.css.Rect;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.RGBImageFilter;
 import java.io.File;
 import java.io.IOException;
 
@@ -25,17 +26,32 @@ public class DensityMapGUI extends Tool {
             "Household Renters"};
     private JComboBox dataList = new JComboBox<>(dataOptions);
     private JButton submitButton = new JButton("SUBMIT");
-    private JLabel aurora = new JLabel(new ImageIcon("img/densityMap/map/aurora.png"));
-    private JLabel eastGwillimbury = new JLabel(new ImageIcon("img/densityMap/map/eastGwillimbury.png"));
-    private JLabel georgina = new JLabel(new ImageIcon("img/densityMap/map/georgina.png"));
-    private JLabel king = new JLabel(new ImageIcon("img/densityMap/map/king.png"));
-    private JLabel markham = new JLabel(new ImageIcon("img/densityMap/map/markham.png"));
-    private JLabel newmarket = new JLabel(new ImageIcon("img/densityMap/map/newmarket.png"));
-    private JLabel richmondHill = new JLabel(new ImageIcon("img/densityMap/map/richmondHill.png"));
-    private JLabel stouffville = new JLabel(new ImageIcon("img/densityMap/map/stouffville.png"));
-    private JLabel vaughan = new JLabel(new ImageIcon("img/densityMap/map/vaughan.png"));
+    private JLabel auroraLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/auroraLabel.png"));
+    private JLabel eastGwillimburyLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/eastGwillimburyLabel.png"));
+    private JLabel georginaLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/georginaLabel.png"));
+    private JLabel kingLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/kingLabel.png"));
+    private JLabel markhamLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/markhamLabel.png"));
+    private JLabel newmarketLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/newmarketLabel.png"));
+    private JLabel richmondHillLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/richmondHillLabel.png"));
+    private JLabel stouffvilleLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/stouffvilleLabel.png"));
+    private JLabel vaughanLabel = new JLabel(new ImageIcon("img/densityMap/map/labels/vaughanLabel.png"));
 
-    private BufferedImage image;
+    private BufferedImage aurora;
+    private BufferedImage eastGwillimbury;
+    private BufferedImage georgina;
+    private BufferedImage king;
+    private BufferedImage markham;
+    private BufferedImage newmarket;
+    private BufferedImage richmondHill;
+    private BufferedImage stouffville;
+    private BufferedImage vaughan;
+    private BufferedImage[] maps = {aurora, eastGwillimbury, georgina, king, markham,
+            newmarket, richmondHill, stouffville, vaughan};
+
+   private String[] mapNames = {"aurora", "eastGwillimbury", "georgina", "king", "markham",
+            "newmarket", "richmondHill", "stouffville", "vaughan"};
+
+    private JLabel[] mapLabels = new JLabel[9];
 
     public DensityMapGUI (int x, int y, int width, int height) {
 
@@ -54,24 +70,32 @@ public class DensityMapGUI extends Tool {
         submitButton.setBackground(new Color(145, 172, 219));
         add(submitButton);
 
-        aurora.setBounds(0, 0, 1366, 768);
-        eastGwillimbury.setBounds(0, 0, 1366, 768);
-        georgina.setBounds(0, 0, 1366, 768);
-        king.setBounds(0, 0, 1366, 768);
-        markham.setBounds(0, 0, 1366, 768);
-        newmarket.setBounds(0, 0, 1366, 768);
-        richmondHill.setBounds(0, 0, 1366, 768);
-        stouffville.setBounds(0, 0, 1366, 768);
-        vaughan.setBounds(0, 0, 1366, 768);
-        add(aurora);
-        add(eastGwillimbury);
-        add(georgina);
-        add(king);
-        add(markham);
-        add(newmarket);
-        add(richmondHill);
-        add(stouffville);
-        add(vaughan);
+        auroraLabel.setBounds(285, 415, 100, 100);
+        add(auroraLabel);
+
+        eastGwillimburyLabel.setBounds(315, 280, 100, 100);
+        add(eastGwillimburyLabel);
+
+        georginaLabel.setBounds(360, 140, 100, 100);
+        add(georginaLabel);
+
+        kingLabel.setBounds(165, 460, 100, 100);
+        add(kingLabel);
+
+        markhamLabel.setBounds(400, 530, 100, 100);
+        add(markhamLabel);
+
+        newmarketLabel.setBounds(280, 360, 100, 100);
+        add(newmarketLabel);
+
+        richmondHillLabel.setBounds(295, 500, 100, 100);
+        add(richmondHillLabel);
+
+        stouffvilleLabel.setBounds(380, 410, 100, 100);
+        add(stouffvilleLabel);
+
+        vaughanLabel.setBounds(200, 580, 100, 100);
+        add(vaughanLabel);
 
         userPanel.setBounds(0, 0, 1366, 768);
         add(userPanel);
@@ -81,8 +105,34 @@ public class DensityMapGUI extends Tool {
         dataList.setForeground(new Color(18, 71, 115));
         add(dataList);
 
+        try {
+            for (int cityMap = 0; cityMap < maps.length; cityMap++) {
+
+                maps[cityMap] = ImageIO.read(new File("img/densityMap/map/lv1/"+mapNames[cityMap]+"Lv1.png"));
+                Image newColouredMap = Toolkit.getDefaultToolkit().createImage(
+                        new FilteredImageSource(maps[cityMap].getSource(),
+                                new RedBlueSwapFilter()));
+
+                mapLabels[cityMap] = new JLabel(new ImageIcon(newColouredMap));
+                mapLabels[cityMap].setBounds(0, 0, 1366, 768);
+                add(mapLabels[cityMap]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         setVisible(true);
 
+    }
+
+    class RedBlueSwapFilter extends RGBImageFilter {
+        public int filterRGB(int x, int y, int rgb) {
+            return ((rgb & 0xFFE5E7E9));
+            // 0xFFE5E7E9 grey
+            // 0xff00ff00
+            //0xFF005CB2
+        }
     }
 
     public String[] getDataOptions() {
