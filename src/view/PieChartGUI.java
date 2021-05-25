50767120116d3e17cc08cc15d218d5583e379c20
Dan18789 <83726063+Dan18789@users.noreply.github.com>
2021-05-25 14:36:04 -0400

    Updates

--- src/view/PieChartGUI.java
+++ src/view/PieChartGUI.java
@@ -1,18 +1,22 @@
 package view;
 
+import javax.swing.*;
+
 public class PieChartGUI extends Tool {
 
     private PieChartPanel[] pieCharts;
 
     public PieChartGUI () {
-        
+
+        //create the pie charts on the frame
         pieCharts = new PieChartPanel[2];
         for (int i = 0; i<pieCharts.length; ++i) {
             pieCharts[i] = new PieChartPanel();
             pieCharts[i].setBounds(0, i*MainFrame.HEIGHT/2, MainFrame.WIDTH, MainFrame.HEIGHT/2);
             add(pieCharts[i]);
         }
-        
+
+
     }
     
     public PieChartPanel getPieCharts (int pieChart) {
--- src/view/PieChartPanel.java
+++ src/view/PieChartPanel.java
@@ -13,14 +13,14 @@ import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 import util.Category;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class PieChartPanel extends JPanel {
     
     private final JButton cityButton = new JButton("SELECT CITY");
-    
+    private static final String PIE_CHART_TITLE_FINAL = "./img/titles/typesOfHousingAndTransportationTitle.png";
+    private final JLabel titleLabel;
+
     private String groupName;
     private ArrayList<Category> dataGroup;
     private DefaultPieDataset<String> displayedData;
@@ -32,6 +32,10 @@ public class PieChartPanel extends JPanel {
         setBorder(BorderFactory.createLineBorder(Color.black));
         cityButton.setBounds(50, MainFrame.HEIGHT/4-25, 200, 50);
         add(cityButton);
+
+        titleLabel = new JLabel(new ImageIcon(PIE_CHART_TITLE_FINAL));
+        titleLabel.setBounds(0, 0, MainFrame.WIDTH, MainFrame.HEIGHT);
+        add(titleLabel);
         
     }
     
@@ -60,15 +64,17 @@ public class PieChartPanel extends JPanel {
     public void createChart (String groupName, String city) {
         
         displayedData = createDataset(city);
-        JFreeChart chart = ChartFactory.createPieChart(groupName, displayedData,
+        JFreeChart chart = ChartFactory.createPieChart(groupName + "" + city, displayedData,
                 true, false, false);
         chart.setBackgroundPaint(Tool.BACKGROUND_COLOUR);
-        
+
+        //check if the chart is null
         if (chartPanel!=null)
             remove(chartPanel);
         chartPanel = new ChartPanel(chart);
-        chartPanel.setBounds(500, 25, 500, MainFrame.HEIGHT/2-100);
+        chartPanel.setBounds(500, 70, 500, MainFrame.HEIGHT/2-100);
         add(chartPanel);
+        //change the actual visual chart
         chartPanel.repaint();
         
     }
@@ -84,4 +90,5 @@ public class PieChartPanel extends JPanel {
     public ArrayList<Category> getDataGroup () {
         return dataGroup;
     }
+
 }
